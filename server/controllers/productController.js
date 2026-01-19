56276e7253669e98124d09c374a15f67cb08a778
vratika1 <vratika6sep@gmail.com>
2026-01-19 22:09:18 +0530

    change

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,6 +1,6 @@
 import {v2 as cloudinary} from "cloudinary";
 import Product from "../models/Product.js";
-import Product from "../models/Product.js";
+
 
 // add product : /api/product/add
 export const addProduct = async (req, res) => {
